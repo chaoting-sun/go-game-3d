f40f46b9c0b7d60b319ee331b14b4345b70144e8
chaoting-sun <chaoting0108@gmail.com>
2024-04-03 15:18:19 +0800

    fix: correct the calculation of pointer.x & pointer.y with varying size

--- src/utils/positionConverter.ts
+++ src/utils/positionConverter.ts
@@ -3,13 +3,23 @@ import { ThreeEvent, useThree } from '@react-three/fiber';
 import * as THREE from 'three';
 import { STONE_Y } from '../goGame/appearance/constants';
 
-function useFixedYPositionConverter() {
+function useFixedYPositionConverter(refId="scene") {
   const { camera } = useThree();
 
   const getPosition = useCallback((event: ThreeEvent<PointerEvent>) => {
     const pointer = new THREE.Vector2();
-    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-    pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    const sceneElement = document.getElementById(refId) as HTMLCanvasElement | null;
+    if (!sceneElement) {
+      throw new Error('Canvas not found');
+    }
+
+    const rect = sceneElement.getBoundingClientRect();
+
+    // get the width and height of canvas
+
+    pointer.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+    pointer.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
 
     // Create a raycaster and set its near and far planes
     const raycaster = new THREE.Raycaster();
