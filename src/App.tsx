812684538d579102287326eb12d15d213493fa4a
chaoting-sun <chaoting0108@gmail.com>
2024-04-04 15:37:54 +0800

    add responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,54 @@ import Scene from "./containers/scene";
 import NavBar from "./containers/navBar";
 import PlayerBlock from "./components/playerBlock";
 
-function App() {
+const App = () => {
   return (
     <div className="flex flex-col h-screen">
       <NavBar />
-      <main className="grow grid grid-cols-1 grid-rows-[1fr,400px] md:grid-cols-[1fr,400px] md:grid-rows-1 gap-[2px] bg-[#242838] ">
+      <main className="grow grid grid-cols-1 sm:grid-cols-[1fr,200px] md:grid-cols-[1fr,400px] lg:grid-cols-[1fr,600px] lg:grid-rows-1 gap-[2px] bg-[#242838]">
         {/* board */}
         <div id="scene-container" className="min-w-0 min-h-0">
           <Scene />
         </div>
         {/* player */}
-        <div id="player-container" className="bg-[#353d57] grid grid-rows-1 grid-cols-2 md:grid-rows-2 md:grid-cols-1">
+        <div
+          id="player-container"
+          className="bg-[#353d57] grid grid-cols-1 grid-rows-[400px,1fr] lg:grid-rows-[250px,1fr]"
+        >
           {/* player block */}
-          <div className="grid grid-rows-2 gap-4 m-4">
-            <PlayerBlock name="Peter Pan" photo="/test-icon.png" rank="2k" captures={8} stoneColor="black" />
-            <PlayerBlock name="Chaoting Sun" photo="/test-icon.png" rank="1k" captures={2} stoneColor="white" />
+          <div className="grid grid-rows-2 grid-cols-1 lg:grid-rows-1 lg:grid-cols-2 gap-4 m-4">
+            <PlayerBlock
+              name="Peter Pan"
+              photo="/test-icon.png"
+              rank="2k"
+              captures={8}
+              stoneColor="black"
+            />
+            <PlayerBlock
+              name="Chaoting Sun"
+              photo="/test-icon.png"
+              rank="1k"
+              captures={2}
+              stoneColor="white"
+            />
           </div>
           {/* chat box */}
-          <div className="rounded-2xl bg-[#4a587f] grid grid-rows-[1fr,60px] m-4">
-            <div id="chatbox-content" className="">
-            </div>
-            <div id="chatbox-input" className="grid grid-cols-[1fr,90px] p-3 gap-3">
+          <div className="rounded-2xl bg-[#4a587f] grid grid-rows-[1fr,120px] md:grid-rows-[1fr,60px] m-4">
+            <div id="chatbox-content" className=""></div>
+            <div
+              id="chatbox-input"
+              className="grid grid-rows-2 grid-cols-1 md:grid-rows-1 md:grid-cols-[1fr,90px] p-3 gap-3"
+            >
               <input type="text" className="rounded p-2 outline-none" />
-              <button className="bg-[#d2d6e5] p-2 flex justify-center items-center rounded">Send</button>
+              <button className="bg-[#d2d6e5] p-2 flex justify-center items-center rounded">
+                Send
+              </button>
             </div>
           </div>
         </div>
-
       </main>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/playerBlock.tsx
+++ src/components/playerBlock.tsx
@@ -8,15 +8,15 @@ const PlayerBlock = ({ name, photo, rank, captures, stoneColor }) => {
 
   return (
     <div className={`rounded-2xl flex justify-evenly items-center ${colorClass}`}>
-      <div className="w-2/5 p-4">
+      <div className="w-4/5 md:w-2/5 p-4">
         <img
           src={photo}
           alt=""
           className="h-[80%] aspect-square object-contain"
         />
       </div>
-      <div className="w-3/5 flex flex-col">
-        <h1 className={`text-2xl ${textColor}`}>{name} [{rank}]</h1>
+      <div className="w-3/5 flex flex-col hidden md:block">
+        <h2 className={`text-xl ${textColor} `}>{name} [{rank}]</h2>
         <p className={`${textColor}`}>{captures} captures</p>
       </div>
     </div>
